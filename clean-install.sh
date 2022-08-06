34a79f2d1a0200effbba9bbb64d3d5920ff3a9b6
frixaco <rustam21ashurmatov@gmail.com>
2022-08-06 13:58:51 +0500

    Bump all packages

--- clean-install.sh
+++ clean-install.sh
@@ -1,5 +1,5 @@
-rm -rf dist node_modules
-rm -rf server/dist server/node_modules
-rm -rf solid/dist solid/node_modules
+rm -rf dist node_modules pnpm-lock.yaml
+rm -rf server/dist server/node_modules server/pnpm-lock.yaml
+rm -rf solid/dist solid/node_modules solid/pnpm-lock.yaml
 
 pnpm install
